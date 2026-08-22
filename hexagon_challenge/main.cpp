ba257ecfe19cf0297b004102d09e6bf71d9b9629
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-08-22 19:12:44 +0100

    hello hexagon

--- hexagon_challenge/main.cpp
+++ hexagon_challenge/main.cpp
@@ -2,11 +2,16 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "shader_s.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+std::vector<float> generateHexagonVertices(float radius, float startAngle);
 void processInput(GLFWwindow* window);
 
-const unsigned int SCR_WIDTH = 1600;
+const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 1200;
 
 int main() {
@@ -32,13 +37,53 @@ int main() {
         return -1;
     }
 
+    // build and compile shader program
+    Shader ourShader("shader.vs", "shader.fs");
+
+    std::vector<float> vertices = generateHexagonVertices(0.5f, 0.0f);
+    unsigned int indices[] = {
+        0, 1, 2,
+        0, 2, 3,
+        0, 3, 4,
+        0, 4, 5,
+        0, 5, 6,
+        0, 6, 1
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // bind the vertex array object first, then bind and set vertex buffers, and then configure vertex attributes
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (!glfwWindowShouldClose(window)) {
         // input
         processInput(window);
 
+        // render
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, 0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -46,6 +91,22 @@ int main() {
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
+}
+
+std::vector<float> generateHexagonVertices(float radius, float startAngle) {
+    std::vector<float> vertices;
+    for (int i = 0; i < 3; i++) {
+        vertices.push_back(0.0f);
+    }
+    for (int i = 0; i < 6; i++) {
+        float angle = startAngle + i * (2.0f * 3.14159265f / 6.0f);
+        float x = radius * cos(angle);
+        float y = radius * sin(angle);
+        vertices.push_back(x);
+        vertices.push_back(y);
+        vertices.push_back(0.0f);
+    }
+    return vertices;
 }
 
 void processInput(GLFWwindow* window) {
