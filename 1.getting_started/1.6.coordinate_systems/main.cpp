76ec446a583dea93cb338edbfadcc6a6f4058252
isaacwilkinsonlongden <isaacwilkinsonlongden@gmail.com>
2026-08-25 19:14:19 +0100

    rotating cube bug fixed

--- 1.getting_started/1.6.coordinate_systems/main.cpp
+++ 1.getting_started/1.6.coordinate_systems/main.cpp
@@ -186,6 +186,8 @@ int main()
     // or set it via the texture class
     ourShader.setInt("texture2", 1);
 
+    glEnable(GL_DEPTH_TEST); 
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -197,7 +199,7 @@ int main()
         // render
         // ------
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // bind textures on corresponding texture units
         glActiveTexture(GL_TEXTURE0);
