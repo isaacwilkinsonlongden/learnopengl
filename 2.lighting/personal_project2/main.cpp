47cd449445a03cf69153ef3f3968a8a2064bdc07
Isaac Wilkinson-Longden <isaacwilkinsonlongden@gmail.com>
2026-09-07 15:57:52 +0100

    personal project lighting

---
+++ 2.lighting/personal_project2/main.cpp
@@ -0,0 +1,267 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "include/shader.h"
+#include "include/camera.h"
+#include "objects/room.h"
+#include "objects/skate_box.h"
+#include "objects/light_bar.h"
+
+#include <iostream>
+
+// struct for creating VAO and VBO for an world object
+struct ObjectBuffers {
+    unsigned int VAO;
+    unsigned int VBO;
+};
+
+void processInput(GLFWwindow *window);
+ObjectBuffers setupObjectBuffers(float vertices[], size_t verticesSize);
+ObjectBuffers setupLightBuffers(float vertices[], size_t verticesSize);
+unsigned int loadTexture(const char* texturePath);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+// settings
+const unsigned int SCR_WIDTH  = 1920;
+const unsigned int SCR_HEIGHT = 1080; 
+
+// camera 
+Camera camera(glm::vec3(0.0f, -1.5f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+
+// timing 
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+int main() {
+    // glfw: initialize and configure
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHintString(GLFW_WAYLAND_APP_ID, "learnopengl");
+
+    // glfw: window creation
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "gallery", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    // tell glfw to capture our mouse 
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: load all OpenGL function pointers
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // configure global OpenGL state
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile our shader program
+    Shader objectShader("shader.vs", "shader.fs");
+    Shader lightShader("light_shader.vs", "light_shader.fs");
+
+    // setup VAO's and VBO's for world objects 
+    ObjectBuffers wallBuffers = setupObjectBuffers(wallVertices, sizeof(wallVertices));
+    ObjectBuffers floorCeilingBuffers = setupObjectBuffers(floorCeilingVertices, sizeof(floorCeilingVertices));
+    ObjectBuffers skateBoxBuffers = setupObjectBuffers(skateBoxVertices, sizeof(skateBoxVertices));
+
+    // setup VAO's and VBO's for light objects
+    ObjectBuffers LightBuffers = setupLightBuffers(lightVertices, sizeof(lightVertices));
+
+    // load and create a texture 
+    stbi_set_flip_vertically_on_load(true);
+    unsigned int textureBrick = loadTexture("Bricks097_1K-JPG_Color.jpg");
+    unsigned int textureConcrete = loadTexture("Concrete042A_1K-JPG_Color.jpg");
+    unsigned int textureWood = loadTexture("Wood039_1K-JPG_Color.jpg");
+
+    // tell OpenGL for each sampler to which texture unit it belongs to
+    objectShader.use();
+    objectShader.setInt("texture1", 0);
+
+    // render loop
+    while (!glfwWindowShouldClose(window)) {
+        // per frame time logic 
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        processInput(window);
+
+        // render
+        glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // pass projection matrix to shader 
+        objectShader.use();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        objectShader.setMat4("projection", projection);
+
+        // camera/view transformation
+        glm::mat4 view = camera.GetViewMatrix();
+        objectShader.setMat4("view", view);
+
+        // model transformation 
+        glBindTexture(GL_TEXTURE_2D, textureBrick);
+        glBindVertexArray(wallBuffers.VAO);
+        objectShader.setMat4("model", glm::mat4(1.0f));
+        glDrawArrays(GL_TRIANGLES, 0, 24);
+        
+        glBindTexture(GL_TEXTURE_2D, textureConcrete);
+        glBindVertexArray(floorCeilingBuffers.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 12);
+
+        glBindTexture(GL_TEXTURE_2D, textureWood);
+        glBindVertexArray(skateBoxBuffers.VAO);
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::translate(model, glm::vec3(0.0f, -2.5f, -4.0f));
+        objectShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 135);
+
+        // draw light object
+        lightShader.use();
+        lightShader.setMat4("projection", projection);
+        lightShader.setMat4("view", view);
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 2.45f, 0.0f));
+        lightShader.setMat4("model", model);
+        glBindVertexArray(LightBuffers.VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        // glfw: swap buffers and poll IO events (keys pressed/ released, mouse moved etc.) 
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // de-allocate all resources once they've outlived their purpose 
+    glDeleteVertexArrays(1, &wallBuffers.VAO);  
+    glDeleteVertexArrays(1, &floorCeilingBuffers.VAO);  
+    glDeleteBuffers(1, &wallBuffers.VBO);
+    glDeleteBuffers(1, &floorCeilingBuffers.VBO);
+
+    // glfw: terminate, clearing all previously allocated glfw resources 
+    glfwTerminate();
+    return 0;
+}
+
+// process all input: query glfw whether relevant keys are pressed/released this frame and react accordingly
+void processInput(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
+        glfwSetWindowShouldClose(window, true);
+    
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// setup VAO and VBO for a world object
+ObjectBuffers setupObjectBuffers(float vertices[], size_t verticesSize) {
+    ObjectBuffers buffers;
+
+    glGenVertexArrays(1, &buffers.VAO);
+    glGenBuffers(1, &buffers.VBO);
+    glBindVertexArray(buffers.VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, buffers.VBO);
+    glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return buffers;
+}
+
+// setup VAO and VBO for a light object
+ObjectBuffers setupLightBuffers(float vertices[], size_t verticesSize) {
+    ObjectBuffers buffers;
+
+    glGenVertexArrays(1, &buffers.VAO);
+    glGenBuffers(1, &buffers.VBO);
+    glBindVertexArray(buffers.VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, buffers.VBO);
+    glBufferData(GL_ARRAY_BUFFER, verticesSize, vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    return buffers;
+}
+
+unsigned int loadTexture(const char* texturePath) {
+    unsigned int texture;
+
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load(texturePath, &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    } else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    return texture;
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves this callback is called 
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls this callback is called 
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
+}
